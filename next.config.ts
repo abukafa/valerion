87f9799fe158433386dcfb31c6bdc9f74a52d533
abukafa <hikam.abukafa@gmail.com>
2026-06-27 15:07:42 +0700

    server.js

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   experimental: {
     serverActions: {
-      bodySizeLimit: '10mb',
+      bodySizeLimit: "10mb",
     },
   },
   // Allow other devices in the same WiFi to access hot reload
-  allowedDevOrigins: ['http://192.168.1.121:3000', '192.168.1.121'],
+  allowedDevOrigins: ["http://192.168.1.121:3000", "192.168.1.121"],
 };
 
 export default nextConfig;
